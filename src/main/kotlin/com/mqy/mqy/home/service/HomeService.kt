c13581a1c86fa1aef645549dbe875dfbe124b068
HI-IR <qq2420226433@outlook.com>
2026-03-30 21:44:58 +0800

    bugfix:修复部分接口HTTP请求不正确，修复无法通过catId查询posts

--- src/main/kotlin/com/mqy/mqy/home/service/HomeService.kt
+++ src/main/kotlin/com/mqy/mqy/home/service/HomeService.kt
@@ -4,6 +4,6 @@ import com.mqy.mqy.home.pojo.vo.GalleryVO
 import com.mqy.mqy.home.pojo.vo.GetPostsVO
 
 interface HomeService {
-	suspend fun getHomePosts(userId: Long, cursor: Long?, limit: Int,keyword: String?): GetPostsVO
-	suspend fun getGallery(cursor: Long?, limit: Int, state: Int?,keyword: String?): GalleryVO
+	suspend fun getHomePosts(userId: Long, catId: Long?, cursor: Long?, limit: Int, keyword: String?): GetPostsVO
+	suspend fun getGallery(cursor: Long?, limit: Int, state: Int?, keyword: String?): GalleryVO
 }
